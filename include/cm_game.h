7ac71522a59dad6d0f55fb0991b0ffe53a066073
crmaykish <crmaykish@gmail.com>
2020-06-21 23:03:43 -0400

    added 60hz update loop

--- include/cm_game.h
+++ include/cm_game.h
@@ -1,11 +1,19 @@
 #ifndef CM_GAME_H
 #define CM_GAME_H
 
+#include <chrono>
+
 namespace cm
 {
+    // Rate at which the game state is updated (not the framerate)
+    const double TICK_RATE_HZ = 60;
+    const auto TIME_PER_TICK = std::chrono::duration<double>(1.0 / TICK_RATE_HZ);
+
     class Game
     {
     private:
+        bool Running = false;
+
     public:
         Game();
         ~Game();
--- src/cm_game.cpp
+++ src/cm_game.cpp
@@ -18,6 +18,31 @@ namespace cm
 
     void Game::Loop()
     {
+        Log("Starting game loop", LOG_INFO);
+
+        Running = true;
+
+        auto previousTime = std::chrono::steady_clock::now();
+        std::chrono::duration<double> lag;
+
+        while (Running)
+        {
+            auto currentTime = std::chrono::steady_clock::now();
+            auto elapsedTime = currentTime - previousTime;
+            previousTime = currentTime;
+            lag += elapsedTime;
+
+            // TODO: handle user input
+
+            while (lag >= TIME_PER_TICK)
+            {
+                // TODO: update game state
+
+                lag -= TIME_PER_TICK;
+            }
+
+            // TODO: render
+        }
     }
 
     void Game::Close()
